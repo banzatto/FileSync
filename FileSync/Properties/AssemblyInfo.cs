59a9b87b151045efcca762694636d0ea4c029cb5
Alexandre Felipe Banzatto <banzatto@bansoft.com.br>
2019-01-07 15:49:56 -0200

    Versão 2.2 recompilado com vs 2019

--- FileSync/Properties/AssemblyInfo.cs
+++ FileSync/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FileSync")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sincronizador de arquivos")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FileSync")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
